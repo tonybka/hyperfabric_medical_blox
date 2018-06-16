1c1ebe9c3ca4c5db0d5e3b3baf91e54d4ed82b34
tonybka <tonybka@gmail.com>
2018-06-16 11:35:04 +0700

    Start building web client

---
+++ web/www/config/express.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import path from 'path';
+import morgan from 'morgan';
+import compression from 'compression';
+
+
+export default app => {
+
+}
